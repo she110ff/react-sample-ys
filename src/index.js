5e00da49e5504774475681408c83162b4424d180
youngsoo <youngsooj@LGDISPLAY.GLOBAL>
2017-08-17 17:41:14 +0900

    flux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Dispatcher from 'flux';
 
 const CountDown = ({count, tick, reset}) => {
   if(count){
